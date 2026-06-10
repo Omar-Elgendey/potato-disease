38934649c759788742361b631598c03f66dad296
Omar Mohamed <omarlegendey68@gmail.com>
2026-06-11 00:09:41 +0300

    initial commit - potato disease project

---
+++ api/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+Model = tf.keras.models.load_model("models/1/model.keras")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+
+@app.get("/ping")
+async def ping():
+    return {"message": "Hello"}
+
+
+def read_file_as_image(data):
+    image = Image.open(BytesIO(data)).convert("RGB")
+    image = image.resize((224, 224))
+    image = np.array(image) / 255.0
+    return image
+
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_data = await file.read()
+    image = read_file_as_image(image_data)
+
+    img_batch = np.expand_dims(image, 0)
+    prediction = Model.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence = float(np.max(prediction[0]))
+
+    return {
+        "predicted_class": predicted_class,
+        "confidence": confidence
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
